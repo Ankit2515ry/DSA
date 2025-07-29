4adbf8aa0590d1c3cbd370d382f7ccdf284750c1
Ankit2515ry <ankit2515ry@gmail.com>
2025-07-29 15:38:49 +0000

    Code challenge submission for question: 2411 Smallest Subarrays With Maximum Bitwise OR

---
+++ 2411 Smallest Subarrays With Maximum Bitwise OR/2411smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res(n);
+        vector<int>bit(32,-1);
+        for(int i=n-1;i>=0;i--){
+            int idx=i;
+            for(int j=0;j<32;j++){
+                if(nums[i]&1<<j)bit[j]=i;
+            }
+            for(int j=0;j<32;j++){
+                idx=max(idx,bit[j]);
+            }
+            res[i]=idx-i+1;
+        }
+        return res;
+    }
+};
