99e37d30fc8634b2440942b63e38c7dc28da72ac
Ankit2515ry <ankit2515ry@gmail.com>
2025-12-28 19:23:29 +0000

    Code challenge submission for question: 721 Accounts Merge

---
+++ 721 Accounts Merge/721accounts-merge.cpp
@@ -0,0 +1,63 @@
+class DSU {
+public:
+    vector<int> parent, size;
+
+    DSU(int n) {
+        parent.resize(n);
+        size.assign(n, 1);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unionBySize(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
+
+        if (size[px] < size[py]) swap(px, py);
+        parent[py] = px;
+        size[px] += size[py];
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        DSU ds(n);
+        unordered_map<string, int> mapMailNode;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if (mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                } else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+        vector<vector<string>> mergedMail(n);
+        for (auto &it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.find(it.second);
+            mergedMail[node].push_back(mail);
+        }
+        vector<vector<string>> ans;
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].empty()) continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for (auto &mail : mergedMail[i])
+                temp.push_back(mail);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
