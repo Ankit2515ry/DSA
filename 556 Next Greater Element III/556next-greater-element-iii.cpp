bec2046e030af78ee1fa7e1f10e894f2955d0f3f
Ankit2515ry <ankit2515ry@gmail.com>
2025-12-29 11:34:07 +0000

    Code challenge submission for question: 556 Next Greater Element III

---
+++ 556 Next Greater Element III/556next-greater-element-iii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s=to_string(n);
+        if(!next_permutation(s.begin(), s.end()))return -1;
+        long long res=stoll(s);
+        if(res>INT_MAX)return -1;
+        return (int)res;
+    }
+};
