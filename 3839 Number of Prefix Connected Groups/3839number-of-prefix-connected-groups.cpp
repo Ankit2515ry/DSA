06d5df724814d3c2713620d4ca9b06d374c8dd22
Ankit2515ry <ankit2515ry@gmail.com>
2026-02-17 21:04:06 +0000

    Code challenge submission for question: 3839 Number of Prefix Connected Groups

---
+++ 3839 Number of Prefix Connected Groups/3839number-of-prefix-connected-groups.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int prefixConnected(vector<string>& words, int k) {
+        unordered_map<string,int>mp;
+        for(int i=0;i<words.size();i++){
+            if(words[i].size()<k)continue;
+            string s=words[i].substr(0,k);
+            mp[s]++;
+        }
+        int res=0;
+        for(auto &it:mp){
+            if(it.second>=2)res++;
+        }
+        return res;
+    }
+};
