d21177830acdd6d9312fa20ad014edb410c4e8e1
Ankit2515ry <ankit2515ry@gmail.com>
2026-05-26 04:51:20 +0000

    Code challenge submission for question: 50 Pow(x, n)

---
+++ 50 Pow(x, n)/50powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        //Binary Exponentiation O(log(n))
+        long long p=n;
+        if(p<0){
+            x=1/x;
+            p=-p;
+        }
+        double res=1;
+        while(p>0){
+            if(p%2==1){
+                res*=x;
+            }
+            x*=x;
+            p/=2;
+        }
+        return res;
+    }
+};
